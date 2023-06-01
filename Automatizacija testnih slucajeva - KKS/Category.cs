b0ea85ce718b83c3b65e7bbe0b1ea9e48ba25618
Ajla Brdarević <93090001+ajla-brdarevic@users.noreply.github.com>
2023-06-01 16:37:48 +0200

    product details

--- Automatizacija testnih slucajeva - KKS/Category.cs
+++ Automatizacija testnih slucajeva - KKS/Category.cs
@@ -1,37 +1,22 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
-using System;
 
 namespace Automatizacija_testnih_slucajeva___KKS
 {
     [TestFixture]
     public class Category
     {
-        private IWebDriver driver;
+        // Može sadržavati null vrijednost
+        private IWebDriver driver = null!;
 
         [OneTimeSetUp]
         public void OneTimeSetup()
         {
-            // Postavljanje WebDrivera i otvaranje stranice samo jednom prije početka svih testova
-            driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("https://www.oreabazaar.com/bs/end_user/auth/login");
-
-            // Unos ispravnih kredencijala za prijavu
-            string email = "ajlabrdarevic@gmail.com";
-            string password = "sifra123";
-
-            // Unos emaila i lozinke
-            driver.FindElement(By.Id("username")).SendKeys(email);
-            driver.FindElement(By.Id("password")).SendKeys(password);
-
-            // Klik na gumb za prijavu
-            driver.FindElement(By.CssSelector(".green.login-register-button")).Click();
-
-            // Pričekaj da se prijaviš i otvori početna stranica
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
-            wait.Until(driver => driver.FindElement(By.Id("user-dropdown-menu")).Displayed);
+            var options = new ChromeOptions();
+            options.PageLoadStrategy = PageLoadStrategy.Normal; // Promjena strategije učitavanja stranice
+            driver = new ChromeDriver(options);
+            driver.Navigate().GoToUrl("https://www.oreabazaar.com");
         }
 
         [OneTimeTearDown]
@@ -42,74 +27,19 @@ namespace Automatizacija_testnih_slucajeva___KKS
         }
 
         [Test]
-        public void CheckCategory_ClothingExists()
+        public void NavigateToCategory_Odjeca_ShouldRedirectToClothingPage()
         {
-            // Provjeri da li je korisnik ulogovan (provjera prisutnosti elementa koji se prikazuje samo kad je korisnik ulogovan)
-            bool isLoggedIn = driver.FindElement(By.Id("user-dropdown-menu")).Displayed;
-            Assert.IsTrue(isLoggedIn, "Korisnik nije ulogovan.");
+            // Klik na "Kategorije"
+            driver.FindElement(By.Id("categories-menu-btn")).Click();
+            /*
+            // Klik na "Odjeća"
+            driver.FindElement(By.LinkText("Odjeća")).Click();
 
-            // Pronađi gumb za kategorije
-            IWebElement categoriesButton = driver.FindElement(By.Id("categories-menu-btn"));
+            // Provjera očekivanog rezultata - provjera prisutnosti elementa na stranici s odjećom
+            bool isDisplayed = driver.FindElement(By.XPath("//h1[contains(text(),'Odjeća')]")).Displayed;
 
-            // Klikni na gumb za kategorije
-            categoriesButton.Click();
-
-            // Pričekaj da se prikažu kategorije
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
-            wait.Until(ExpectedConditions.ElementExists(By.CssSelector(".bmenu.menu-panel")));
-
-            // Pronađi kategoriju odjeće
-            IWebElement clothingCategory = driver.FindElement(By.XPath("//a[contains(text(), 'Odjeća')]"));
-
-            // Provjeri da li je kategorija odjeće prisutna
-            Assert.IsTrue(clothingCategory.Displayed, "Kategorija odjeće nije prisutna.");
-
-            // Klikni na kategoriju odjeće
-            clothingCategory.Click();
-
-            // Provjeri da li se otvorila kategorija odjeće
-            string expectedUrl = "https://www.oreabazaar.com/bs/category/1/odjeca";
-            string actualUrl = driver.Url;
-            Assert.AreEqual(expectedUrl, actualUrl, "Kategorija odjeće se nije otvorila.");
-
-            // Provjeri naslov kategorije odjeće
-            IWebElement categoryTitle = driver.FindElement(By.XPath("//h1[contains(text(), 'Odjeća')]"));
-            Assert.IsTrue(categoryTitle.Displayed, "Naslov kategorije odjeće nije prikazan.");
-        }
-        [Test]
-        public void CheckCategory_ClothingExists()
-        {
-            // Provjeri da li je korisnik ulogovan (provjera prisutnosti elementa koji se prikazuje samo kad je korisnik ulogovan)
-            bool isLoggedIn = driver.FindElement(By.Id("user-dropdown-menu")).Displayed;
-            Assert.IsTrue(isLoggedIn, "Korisnik nije ulogovan.");
-
-            // Pronađi gumb za kategorije
-            IWebElement categoriesButton = driver.FindElement(By.Id("categories-menu-btn"));
-
-            // Klikni na gumb za kategorije
-            categoriesButton.Click();
-
-            // Pričekaj da se prikažu kategorije
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
-            wait.Until(ExpectedConditions.ElementExists(By.CssSelector(".bmenu.menu-panel")));
-
-            // Pronađi kategoriju odjeće
-            IWebElement clothingCategory = driver.FindElement(By.XPath("//a[contains(text(), 'Odjeća')]"));
-
-            // Provjeri da li je kategorija odjeće prisutna
-            Assert.IsTrue(clothingCategory.Displayed, "Kategorija odjeće nije prisutna.");
-
-            // Klikni na kategoriju odjeće
-            clothingCategory.Click();
-
-            // Provjeri da li se otvorila kategorija odjeće
-            string expectedUrl = "https://www.oreabazaar.com/bs/category/1/odjeca";
-            string actualUrl = driver.Url;
-            Assert.AreEqual(expectedUrl, actualUrl, "Kategorija odjeće se nije otvorila.");
-
-            // Provjeri naslov kategorije odjeće
-            IWebElement categoryTitle = driver.FindElement(By.XPath("//h1[contains(text(), 'Odjeća')]"));
-            Assert.IsTrue(categoryTitle.Displayed, "Naslov kategorije odjeće nije prikazan.");
+            // Provjera očekivanog rezultata
+            Assert.IsTrue(isDisplayed, "Korisnik nije preusmjeren na stranicu s odjećom!");*/
         }
     }
 }
---
+++ Automatizacija testnih slucajeva - KKS/ProductDetails.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Automatizacija_testnih_slucajeva___KKS
+{
+    [TestFixture]
+    public class ProductDetails
+    {
+        private IWebDriver driver = null!;
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.oreabazaar.com");
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTeardown()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        public void NavigateToProductDetails_ShouldShowProductDetails()
+        {
+            // Navigacija na "oreabazaar.com"
+            driver.Navigate().GoToUrl("https://www.oreabazaar.com");
+
+            // Navigacija na "Božićni tanjuri"
+            driver.FindElement(By.LinkText("Božićni tanjuri")).Click();
+
+            // Klik na određeni proizvod
+            driver.FindElement(By.LinkText("Božićni tanjuri")).Click();
+
+            // Provjera očekivanog rezultata - provjera prisutnosti elemenata s detaljima o proizvodu
+            bool areDetailsShown = driver.FindElement(By.ClassName("product-details")).Displayed;
+
+            // Provjera očekivanog rezultata
+            Assert.IsTrue(areDetailsShown, "Detalji proizvoda nisu prikazani!");
+        }
+    }
+}
